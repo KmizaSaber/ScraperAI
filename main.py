70f6d6f66bfa3c20accf4d18f025b2ddd1624d45
KmizaSaber <96543122+KmizaSaber@users.noreply.github.com>
2024-01-03 11:44:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -39,6 +39,7 @@ with tab2:
     transcript = lch.speech_to_text(audio)
     with st.container(border=True):
       st.title("Risposta:")
+      st.download_button(transcript)
       st.text(textwrap.fill(transcript)) 
 
 with tab3:
